40edeb723add94d1fdac020dec92fdd0c9dd2bc9
zhselfweb <151199161+zhselfweb@users.noreply.github.com>
2024-05-04 06:36:39 +0800

    Update ytplayer.js

--- ytplayer/ytplayer.js
+++ ytplayer/ytplayer.js
@@ -11,6 +11,12 @@ $(document).ready(function() {
     });
 });
 
+document.getElementById("back-to-top-btn").addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 
 
 function playVideo() {
