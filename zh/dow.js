8fe9fb0505dd80bfa650f5dd59fe325084360074
zhselfweb <151199161+zhselfweb@users.noreply.github.com>
2024-08-25 23:56:00 +0800

    Delete zh directory

--- zh/dow.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = 'zh.zip';
-    link.download = '𝓏𝒽ℴ𝓊-名片源碼😎.zip';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
