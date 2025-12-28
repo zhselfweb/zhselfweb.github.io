a93165001c4d6cdd8bb4fce169209fd4040efa60
zhselfweb <zhselfweb@gmail.com>
2025-12-29 00:20:05 +0800

    Upload 108 Exam files to exam folder

---
+++ exam/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'v2';
+const ASSETS = [
+  './home.html',
+  './manifest.json',
+  './icon.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force waiting service worker to become active
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // Claim clients immediately
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
