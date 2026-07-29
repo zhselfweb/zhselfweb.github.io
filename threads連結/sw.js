782a971656ffd8d8b39267b8c396f2d2f5ee48d2
zhselfweb <151199161+zhselfweb@users.noreply.github.com>
2026-07-30 02:01:23 +0800

    Update sw.js

--- threads連結/sw.js
+++ threads連結/sw.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', (event) => {
   // 保持簡單的快取或直接放行
 });
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
